f898fd48d3a76bb1ea032b72a7e207f4798f343f
Vaibhav Sahu <vaibhavsahu200@gmail.com>
2024-06-04 12:26:43 +0530

    changed some css in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,37 @@
 import React, { useState } from 'react'
 
 function Header() {
-    const [menu, setMenu] = useState(true);
+const [menu, setMenu] = useState(true);
 
-    function changeMenu(){
-        setMenu(!menu);
-    }
+function changeMenu(){
+    setMenu(!menu);
+}
 
   return (
-    <nav className='flex px-[40px] py-[30px] justify-between items-center'>
+    <nav className='flex px-[20px] py-[15px] md:px-[40px] md:py-[30px] justify-between items-center'>
         <div className='flex justify-center items-center gap-[30px]'>
-            <div className='flex justify-center items-center gap-[8px]'>
+            <a href='/' className='flex justify-center items-center gap-[8px]'>
                 <img className='h-[34px] w-[34px]' src="/Images/logo.png" alt="logo" />
                     <h2 className='text-[26px] font-bold flex justify-start items-center'>uifry <span className='text-[12px]'>&#8482;</span></h2>
-            </div>
+            </a>
             <ul className='hidden md:flex text-[20px] justify-center items-center gap-[20px]'>
-                <li>
+                <li className='hover:text-[#FF5555] font-semibold'>
                     <a href="/">Home</a>
                 </li>
-                <li>
+                <li className='hover:text-[#FF5555] font-semibold'>
                     <a href="#features">Features</a>
                 </li>
-                <li>
+                <li className='hover:text-[#FF5555] font-semibold'>
                     <a href="#testimonials">Testimonials</a>
                 </li>
-                <li>
+                <li className='hover:text-[#FF5555] font-semibold'>
                     <a href="#pricing">Pricing</a>
                 </li>
             </ul>
         </div>
         <div className='flex justify-center items-center gap-[20px] relative'>
             <div className='text-[15px]'>
-                <a className='bg-black text-white px-[40px] py-[15px] rounded-[8px]' href="">Download</a>
+                <a className='bg-black text-white px-[20px] py-[12px] md:px-[40px] md:py-[15px]  rounded-[8px]' href="">Download</a>
             </div>
             <div onClick={changeMenu} className='flex md:hidden h-[30px] w-[30px] cursor-pointer'>
                 <img className={menu ? `block` : `hidden`} src="/Images/menu.svg" alt="menu-open" />
